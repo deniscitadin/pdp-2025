1f2dcd36efd3b0bbd25595670f3602746b11177d
deniscitadin <deniscitadin@gmail.com>
2024-10-29 21:16:28 -0300

    first commit

--- kubernetes-module/main.tf
+++ kubernetes-module/main.tf
@@ -1,8 +1,10 @@
 module "app_deploy" {
-  source        = "./module/kubernetes_job" #Required
-  app_path      = "./my/path/toapp"         #Required
-  cpu_limit     = 2                         #Required
-  build_command = "my build command"        #Optional
-  run_command   = "my run command"          #Optional
-  workdir       = "/usr/src/app"            #Optional
+  source          = "./module/kubernetes_job"   #Required
+  app_path        = "./my/path/toapp"           #Required
+  cpu_limit       = 2                           #Required
+  build_command   = "my build command"          #Optional
+  run_command     = "my run command"            #Optional
+  workdir         = "/usr/src/app"              #Optional
+  custom_image    = "my-custom-image"           #Optional
+  kubeconfig_path = "my-kubeconfig-path"        #Optional
 }
